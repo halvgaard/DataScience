2471f54b5fcc7e43668f7c57bd7909d9b452c1ec
Lukas Rasocha <71828785+lukyrasocha@users.noreply.github.com>
2022-06-22 17:20:27 +0200

    Task8 - Create a ZenML pipeline downloading data from BigQuery in one step in local stack (#23)
    * Create a ZenML pipeline downloading toy data from BQ

---
+++ Task8/zenml_project/zenml_project/pipeline.py
@@ -0,0 +1,79 @@
+from zenml.pipelines import pipeline
+from zenml.steps import step, Output, BaseStepConfig
+from zenml.repository import Repository
+import pandas_gbq
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.base import ClassifierMixin
+from sklearn.linear_model import LogisticRegression
+
+step_operator = Repository().active_stack.step_operator
+
+class BigQueryImporterConfig(BaseStepConfig):
+    query: str = 'SELECT * FROM `computas_dataset.iris_data`'
+    project_id: str = 'computas-project-345810'
+
+@step
+def bigquery_importer(config: BigQueryImporterConfig) -> pd.DataFrame:
+    return pandas_gbq.read_gbq(config.query, project_id = config.project_id)
+
+@step
+def preparator(data: pd.DataFrame) -> Output(
+    X_train=np.ndarray, X_test=np.ndarray, y_train=np.ndarray, y_test=np.ndarray
+):
+    encoder = OrdinalEncoder()
+    data['target'] = encoder.fit_transform(data[['Species']])
+
+    X = np.array(data[['SepalLengthCm','SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']])
+    y = np.array(data['target'])
+
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
+    print(X_train)
+
+    return X_train, X_test, y_train, y_test 
+
+@step(custom_step_operator=step_operator.name)
+def trainer(
+    X_train: np.ndarray,
+    y_train: np.ndarray,
+) -> LogisticRegression:
+    """Train a simple sklearn classifier for the Iris dataset."""
+    print('Training')
+    model = LogisticRegression(max_iter=500)
+    model.fit(X_train, y_train)
+    return model
+
+@step
+def evaluator(
+    X_test: np.ndarray,
+    y_test: np.ndarray,
+    model: LogisticRegression,
+) -> float:
+    """Calculate the accuracy on the test set"""
+    test_acc = model.score(X_test, y_test)
+    print(f"Test accuracy: {test_acc}")
+    return test_acc
+    
+@pipeline
+def iris_pipeline(
+    bigquery_importer,
+    preparator,
+    trainer,
+    evaluator,
+):
+    """Links all the steps together in a pipeline"""
+    data = bigquery_importer() 
+    X_train, X_test, y_train, y_test = preparator(data=data)
+    model = trainer(X_train=X_train, y_train=y_train)
+    evaluator(X_test=X_test, y_test=y_test, model=model)
+
+pipeline = iris_pipeline(
+    bigquery_importer=bigquery_importer(),
+    preparator=preparator(),
+    trainer=trainer(),
+    evaluator=evaluator(),
+)
+
+pipeline.run()
